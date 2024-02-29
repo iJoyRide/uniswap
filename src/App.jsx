b24c9cde6cb385d2759bdc2aa593fcd77a5f6ad7
iJoyRide <sumfeis3@gmail.com>
2024-03-01 00:26:12 +0800

    still stuck to the left

--- src/App.jsx
+++ src/App.jsx
@@ -74,15 +74,14 @@ function App() {
   
 
   const boxStyles = {
-    background: "#fdfdfd",
-    marginTop: "10%",
+    background: "#E2E5DE",
+    marginTop: "10rem",
     textAlign: "center",
     color: "#222",
     minHeight: "20rem",
     borderRadius: 2,
     padding: "4rem 2rem",
-    boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)",
-    position: "relative"
+    boxShadow: "30px 40px 10px 9px rgba(0,0,0,0.1)"
   };
 
   return (
